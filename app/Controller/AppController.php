e8128e99b3a14c9f0b5d8ca463412c0ab78138bb
nirasan <niranirao@gmail.com>
2014-02-26 14:02:00 +0900

    認証の実装

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,12 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'clicks', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
 }
---
+++ app/Controller/ClicksController.php
@@ -0,0 +1,8 @@
+<?php
+App::uses('AppController', 'Controller');
+class ClicksController extends AppController {
+    
+    public function index() {
+    
+    }
+}
